762a5176a50d990e91ec350bb684e95d46b9a3ef
sakamuuy <sakamuu.y@gmail.com>
2022-12-05 22:57:20 +0900

    Add terminal and welcome message

--- src/editor.rs
+++ src/editor.rs
@@ -1,16 +1,15 @@
-use std::io::{self, stdout, Write};
+use crate::Terminal;
 use termion::event::Key;
-use termion::input::TermRead;
-use termion::raw::IntoRawMode;
+
+const VERSION: &str = env!("CARGO_PKG_VERSION");
 
 pub struct Editor {
     should_quit: bool,
+    terminal: Terminal
 }
 
 impl Editor {
     pub fn run(&mut self) {
-        let _stdout = stdout().into_raw_mode().unwrap();
-        
         loop {
             if let Err(error) = self.refresh_screen() {
                 die(error);
@@ -25,16 +24,52 @@ impl Editor {
     }
 
     pub fn default() -> Self {
-        Self { should_quit: false }
+        Self { 
+            should_quit: false,
+            terminal: Terminal::default().expect("Failed to initialize terminal"),
+        }
     }
 
     fn refresh_screen(&self) -> Result<(), std::io::Error> {
-        print!("{}", termion::clear::All);
-        io::stdout().flush()
+        Terminal::cursor_hide();
+        Terminal::cursor_position(0, 0);
+        if self.should_quit {
+            Terminal::clear_screen();
+            println!("Goodbye.\r");
+        } else {
+            self.draw_rows();
+            Terminal::cursor_position(0, 0);
+        }
+        Terminal::cursor_show();
+        Terminal::flush()
+    }
+
+    fn draw_welcom_message(&self) {
+        let mut welcome_message = format!("Hecto editor -- version {}", VERSION);
+        let width = self.terminal.size().width as usize;
+        let len = welcome_message.len();
+        let padding = width.saturating_sub(len) / 2;
+        let spaces = " ".repeat(padding.saturating_sub(1));
+        welcome_message = format!("~{}{}", spaces, welcome_message);
+        welcome_message.truncate(width);
+        println!("{}\r", welcome_message);
+    }
+
+    fn draw_rows(&self) {
+        let height = self.terminal.size().height;
+        for row in 0..height - 1 {
+            Terminal::clear_current_line();
+
+            if row == height / 3 {
+                self.draw_welcom_message();
+            } else {
+                println!("~\r");
+            }
+        }
     }
 
     fn process_keypress(&mut self) -> Result<(), std::io::Error> {
-        let pressed_keys = read_key()?;
+        let pressed_keys = Terminal::read_key()?;
         match pressed_keys {
             Key::Ctrl('q') => self.should_quit = true,
             _ => (),
@@ -43,14 +78,7 @@ impl Editor {
     }
 }
 
-fn read_key() -> Result<Key, std::io::Error> {
-    loop {
-        if let Some(key) = io::stdin().lock().keys().next() {
-            return key;
-        }
-    }
-}
-
 fn die(e: std::io::Error) {
+    Terminal::clear_screen();
     panic!("{}", e);
 }
