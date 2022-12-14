b9cfce2ed4be03c642755be74dfb8b75335a50b3
sakamuuy <sakamuu.y@gmail.com>
2022-12-14 09:54:49 +0900

    Opne file with filename from args

--- src/document.rs
+++ src/document.rs
@@ -1,14 +1,27 @@
 use crate::Row;
+use std::fs;
 
 #[derive(Default)]
-pub struct Document{
-  rows: Vec<Row>,
+pub struct Document {
+    rows: Vec<Row>,
 }
 
 impl Document {
-  pub fn open() -> Self {
-    let mut rows = Vec::new();
-    rows.push(Row::from("Hello, world"));
-    Self { rows }
-  }
+    pub fn open(file_name: &str) -> Result<Self, std::io::Error> {
+        let contents = fs::read_to_string(file_name)?;
+        let mut rows = Vec::new();
+
+        for value in contents.lines() {
+            rows.push(Row::from(value))
+        }
+        Ok(Self { rows })
+    }
+
+    pub fn row(&self, index: usize) -> Option<&Row> {
+        self.rows.get(index)
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.rows.is_empty()
+    }
 }
